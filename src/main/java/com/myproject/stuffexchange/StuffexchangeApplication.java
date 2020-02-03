3fc9beb95705921b3898c04fbaee357dee56eb62
Sáfár Péter <safpeter03@gmail.com>
2020-02-03 17:40:46 +0100

    initial commit

---
+++ src/main/java/com/myproject/stuffexchange/StuffexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.stuffexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuffexchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuffexchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myproject/stuffexchange/StuffexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.stuffexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuffexchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
